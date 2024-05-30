681b39ef3b715ae7250703c70a8d0ba80409edb0
Evan Cobb <evan.cobb@pm.me>
2024-05-30 18:10:25 -0500

    makes it "easier" to add more dimensions/portals, solidifies "test-dimension" -> desert

--- src/main/java/com/example/icbat/biomedimensionsexpanded/BiomeDimensionsExpanded.java
+++ src/main/java/com/example/icbat/biomedimensionsexpanded/BiomeDimensionsExpanded.java
@@ -2,12 +2,19 @@ package com.example.icbat.biomedimensionsexpanded;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.resource.ResourceFinder;
+import net.minecraft.resource.fs.ResourceFileSystem;
+import net.minecraft.util.DyeColor;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import net.kyrptonaught.customportalapi.api.CustomPortalBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BiomeDimensionsExpanded implements ModInitializer {
 	public static final String MOD_ID = "biome-dimensions-expanded";
@@ -21,12 +28,22 @@ public class BiomeDimensionsExpanded implements ModInitializer {
 
 		LOGGER.info("Hello Fabric world!");
 
+		List<PortalToDimension> portals = new ArrayList<>();
 
-		CustomPortalBuilder.beginPortal()
-			.frameBlock(Blocks.BAMBOO_BLOCK)
-			.destDimID(new Identifier(MOD_ID, "test-dimension"))
-			.tintColor(33, 33, 33)
-			.onlyLightInOverworld()
-			.registerPortal();
+		portals.add(new PortalToDimension(Blocks.SANDSTONE, "desert",  174, 119, 31));
+
+
+		for (PortalToDimension portal : portals) {
+			CustomPortalBuilder.beginPortal()
+					.frameBlock(portal.portal_block())
+					.destDimID(new Identifier(MOD_ID, portal.dimension_id()))
+					.tintColor(portal.portal_r(), portal.portal_g(), portal.portal_b())
+					.onlyLightInOverworld()
+					.registerPortal();
+		}
 	}
+}
+
+record PortalToDimension(Block portal_block, String dimension_id, int portal_r, int portal_g, int portal_b) {
+
 }
