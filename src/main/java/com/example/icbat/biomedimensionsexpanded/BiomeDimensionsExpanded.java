825c5ab26d960423ea74b98bef9a188ec516d585
Evan Cobb <evan.cobb@pm.me>
2024-05-30 10:07:59 -0500

    pulls in CustomPortalApi and uses it to try and setup a new portal type to the nether

--- src/main/java/com/example/icbat/biomedimensionsexpanded/BiomeDimensionsExpanded.java
+++ src/main/java/com/example/icbat/biomedimensionsexpanded/BiomeDimensionsExpanded.java
@@ -2,8 +2,12 @@ package com.example.icbat.biomedimensionsexpanded;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.minecraft.block.Blocks;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import net.kyrptonaught.customportalapi.api.CustomPortalBuilder;
 
 public class BiomeDimensionsExpanded implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
@@ -18,5 +22,13 @@ public class BiomeDimensionsExpanded implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
+
+		CustomPortalBuilder.beginPortal()
+				.frameBlock(Blocks.BAMBOO_BLOCK)
+				.lightWithWater()
+				.destDimID(new Identifier("the_nether"))
+				.tintColor(33, 33, 33)
+				.onlyLightInOverworld()
+				.registerPortal();
 	}
 }
