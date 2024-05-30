ffd9ed91bcade0b8ede56537db66ccfcd866a5b1
Evan Cobb <evan.cobb@pm.me>
2024-05-29 20:57:34 -0500

    initial commit - template from Fabric's project generator

---
+++ src/client/java/com/example/icbat/biomedimensionsexpanded/BiomeDimensionsExpandedClient.java
@@ -0,0 +1,10 @@
+package com.example.icbat.biomedimensionsexpanded;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BiomeDimensionsExpandedClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
