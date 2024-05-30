6ff8c5b4399139c6df28b7419476206e0833ce85
Evan Cobb <evan.cobb@pm.me>
2024-05-30 13:49:13 -0500

    wires up datagen for one dimension

--- src/main/java/com/example/icbat/biomedimensionsexpanded/BiomeDimensionsExpandedDataGenerator.java
+++ src/main/java/com/example/icbat/biomedimensionsexpanded/BiomeDimensionsExpandedDataGenerator.java
@@ -1,11 +1,21 @@
 package com.example.icbat.biomedimensionsexpanded;
 
+import com.example.icbat.biomedimensionsexpanded.datagen.ModWorldGenerator;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.minecraft.registry.RegistryBuilder;
+import net.minecraft.registry.RegistryKeys;
 
 public class BiomeDimensionsExpandedDataGenerator implements DataGeneratorEntrypoint {
     @Override
     public void onInitializeDataGenerator(FabricDataGenerator generator) {
         FabricDataGenerator.Pack pack = generator.createPack();
+
+        pack.addProvider(ModWorldGenerator::new);
+    }
+
+    @Override
+    public void buildRegistry(RegistryBuilder registryBuilder) {
+        registryBuilder.addRegistry(RegistryKeys.DIMENSION_TYPE, OneDimension::bootstrapType);
     }
 }
---
+++ src/main/java/com/example/icbat/biomedimensionsexpanded/OneDimension.java
@@ -0,0 +1,42 @@
+package com.example.icbat.biomedimensionsexpanded;
+
+import net.minecraft.registry.Registerable;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
+import net.minecraft.world.World;
+import net.minecraft.world.dimension.DimensionOptions;
+import net.minecraft.world.dimension.DimensionType;
+import net.minecraft.world.dimension.DimensionTypes;
+
+import java.util.OptionalLong;
+
+public class OneDimension {
+    public static final RegistryKey<DimensionOptions> KAUPENDIM_KEY = RegistryKey.of(RegistryKeys.DIMENSION,
+            new Identifier(BiomeDimensionsExpanded.MOD_ID, "test-dimension"));
+    public static final RegistryKey<World> KAUPENDIM_LEVEL_KEY = RegistryKey.of(RegistryKeys.WORLD,
+            new Identifier(BiomeDimensionsExpanded.MOD_ID, "test-dimension"));
+    public static final RegistryKey<DimensionType> KAUPEN_DIM_TYPE = RegistryKey.of(RegistryKeys.DIMENSION_TYPE,
+            new Identifier(BiomeDimensionsExpanded.MOD_ID, "test-dimension_type"));
+
+    public static void bootstrapType(Registerable<DimensionType> context) {
+        context.register(KAUPEN_DIM_TYPE, new DimensionType(
+                OptionalLong.of(12000), // fixedTime
+                false, // hasSkylight
+                false, // hasCeiling
+                false, // ultraWarm
+                true, // natural
+                1.0, // coordinateScale
+                true, // bedWorks
+                false, // respawnAnchorWorks
+                0, // minY
+                256, // height
+                256, // logicalHeight
+                BlockTags.INFINIBURN_OVERWORLD, // infiniburn
+                DimensionTypes.OVERWORLD_ID, // effectsLocation
+                1.0f, // ambientLight
+                new DimensionType.MonsterSettings(false, false, UniformIntProvider.create(0, 0), 0)));
+    }
+}
