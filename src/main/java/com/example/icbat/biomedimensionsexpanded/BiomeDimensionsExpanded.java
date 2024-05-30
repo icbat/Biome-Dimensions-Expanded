9d02f2b7a0ba81820ca1b3e2ca125e6ae597d183
Evan Cobb <evan.cobb@pm.me>
2024-05-30 15:12:57 -0500

    removes data-gen for dimension-types
    We're just going to re-use the pre-built ones anyways

--- src/main/java/com/example/icbat/biomedimensionsexpanded/BiomeDimensionsExpanded.java
+++ src/main/java/com/example/icbat/biomedimensionsexpanded/BiomeDimensionsExpanded.java
@@ -3,11 +3,15 @@ package com.example.icbat.biomedimensionsexpanded;
 import net.fabricmc.api.ModInitializer;
 
 import net.minecraft.block.Blocks;
+import net.minecraft.util.DyeColor;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import net.kyrptonaught.customportalapi.api.CustomPortalBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class BiomeDimensionsExpanded implements ModInitializer {
 	public static final String MOD_ID = "biome-dimensions-expanded";
@@ -21,9 +25,9 @@ public class BiomeDimensionsExpanded implements ModInitializer {
 
 		LOGGER.info("Hello Fabric world!");
 
+
 		CustomPortalBuilder.beginPortal()
 				.frameBlock(Blocks.BAMBOO_BLOCK)
-				.lightWithWater()
 				.destDimID(new Identifier(MOD_ID, "test-dimension"))
 				.tintColor(33, 33, 33)
 				.onlyLightInOverworld()
