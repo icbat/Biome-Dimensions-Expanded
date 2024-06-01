f41c4984dd4a8bfb66dad5a1fc7484005a875007
Evan Cobb <evan.cobb@pm.me>
2024-06-01 14:51:15 -0500

    adds snowy/icy biomes, internally uses Java Color for better IDE support
    portal colors show up in Intellij now
    
    so it's easier to tell if I forgot to change the values

--- src/main/java/com/example/icbat/biomedimensionsexpanded/BiomeDimensionsExpanded.java
+++ src/main/java/com/example/icbat/biomedimensionsexpanded/BiomeDimensionsExpanded.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 
 import net.kyrptonaught.customportalapi.api.CustomPortalBuilder;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,17 +24,33 @@ public class BiomeDimensionsExpanded implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 		List<PortalToDimension> portals = new ArrayList<>();
-		portals.add(new PortalToDimension(Blocks.SANDSTONE, "beach",  174, 119, 31));
-		portals.add(new PortalToDimension(Blocks.CHISELED_SANDSTONE, "desert",  174, 119, 31));
-		portals.add(new PortalToDimension(Blocks.RED_MUSHROOM_BLOCK, "mooshroom",  122, 32, 26));
+		portals.add(new PortalToDimension(Blocks.SANDSTONE, "beach",  new Color(147, 71, 129)));
+		portals.add(new PortalToDimension(Blocks.CHISELED_SANDSTONE, "desert",  new Color(174, 119, 31)));
+		portals.add(new PortalToDimension(Blocks.RED_MUSHROOM_BLOCK, "mooshroom",  new Color(122, 32, 26)));
 
-		portals.add(new PortalToDimension(Blocks.STRIPPED_BAMBOO_BLOCK, "bamboo_jungle",  104, 114, 32));
-		portals.add(new PortalToDimension(Blocks.STRIPPED_MANGROVE_LOG, "swamp",  91, 58, 35));
-		portals.add(new PortalToDimension(Blocks.STRIPPED_BIRCH_LOG, "birch",  162, 162, 130));
-		portals.add(new PortalToDimension(Blocks.STRIPPED_DARK_OAK_LOG, "dark_forest",  57, 40, 29));
-		portals.add(new PortalToDimension(Blocks.STRIPPED_CHERRY_LOG, "cherry_grove",  153, 75, 124));
-		portals.add(new PortalToDimension(Blocks.STRIPPED_ACACIA_LOG, "savanna",  147, 60, 23));
-		portals.add(new PortalToDimension(Blocks.STRIPPED_JUNGLE_LOG, "jungle",  147, 60, 23));
+		portals.add(new PortalToDimension(Blocks.STRIPPED_BAMBOO_BLOCK, "bamboo_jungle",  new Color(104, 114, 32)));
+		portals.add(new PortalToDimension(Blocks.STRIPPED_MANGROVE_LOG, "swamp",  new Color(91, 58, 35)));
+		portals.add(new PortalToDimension(Blocks.STRIPPED_BIRCH_LOG, "birch",  new Color(162, 162, 130)));
+		portals.add(new PortalToDimension(Blocks.STRIPPED_DARK_OAK_LOG, "dark_forest",  new Color(57, 40, 29)));
+		portals.add(new PortalToDimension(Blocks.STRIPPED_CHERRY_LOG, "cherry_grove",  new Color(153, 75, 124)));
+		portals.add(new PortalToDimension(Blocks.STRIPPED_ACACIA_LOG, "savanna",  new Color(147, 60, 23)));
+		portals.add(new PortalToDimension(Blocks.STRIPPED_JUNGLE_LOG, "jungle",  new Color(80, 112, 45)));
+
+		portals.add(new PortalToDimension(Blocks.SNOW_BLOCK, "snowy_plains",  new Color(200, 205, 204)));
+		portals.add(new PortalToDimension(Blocks.POWDER_SNOW, "snowy_slopes",  new Color(201, 206, 205)));
+		portals.add(new PortalToDimension(Blocks.ICE, "grove",  new Color(127, 180, 196)));
+		portals.add(new PortalToDimension(Blocks.PACKED_ICE, "ice_spikes",  new Color(87, 137, 150)));
+		portals.add(new PortalToDimension(Blocks.BLUE_ICE, "frozen_ocean",  new Color(47, 145, 176)));
+
+		// Honey Block -> Meadow, Flower Forest
+		// Seaweed Block -> Deep Ocean
+		// Slime Block -> ???
+		// Stripped Pine Block -> ???
+		// ??? -> Badlands
+		// ??? -> Dripstone
+		// ??? -> Lush Caves
+		// ??? -> Mountain Biomes?
+		// ??? -> Sunflower Fields?
 
         LOGGER.info("Adding {} biome portals", portals.size());
 
@@ -42,7 +59,7 @@ public class BiomeDimensionsExpanded implements ModInitializer {
 			CustomPortalBuilder.beginPortal()
 					.frameBlock(portal.portal_block())
 					.destDimID(new Identifier(MOD_ID, portal.dimension_id()))
-					.tintColor(portal.portal_r(), portal.portal_g(), portal.portal_b())
+					.tintColor(portal.portal_color().getRed(), portal.portal_color().getBlue(), portal.portal_color().getGreen())
 					.onlyLightInOverworld()
 					.registerPortal();
 		}
@@ -55,4 +72,4 @@ public class BiomeDimensionsExpanded implements ModInitializer {
  * @param portal_block the Minecraft block with which to make the portal from
  * @param dimension_id string must exactly match the filename for the corresponding dimension in resources/data/<>/dimension
  * */
-record PortalToDimension(Block portal_block, String dimension_id, int portal_r, int portal_g, int portal_b) {}
+record PortalToDimension(Block portal_block, String dimension_id, Color portal_color) {}
