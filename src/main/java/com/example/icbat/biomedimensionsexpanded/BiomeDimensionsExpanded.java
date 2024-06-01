f108d1e1ecd42f68f2c941ff449a06676a5cade7
Evan Cobb <evan.cobb@pm.me>
2024-06-01 14:29:51 -0500

    adds birch, swamp, and dark_forest dimensions. changes bamboo portal to stripped bamboo
    to better match the other wood portals

--- src/main/java/com/example/icbat/biomedimensionsexpanded/BiomeDimensionsExpanded.java
+++ src/main/java/com/example/icbat/biomedimensionsexpanded/BiomeDimensionsExpanded.java
@@ -22,14 +22,16 @@ public class BiomeDimensionsExpanded implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
-		// TODO Can we make a file for this, so it's not Java-y to write more?
 		List<PortalToDimension> portals = new ArrayList<>();
 		portals.add(new PortalToDimension(Blocks.SANDSTONE, "beach",  174, 119, 31));
 		portals.add(new PortalToDimension(Blocks.CHISELED_SANDSTONE, "desert",  174, 119, 31));
 		portals.add(new PortalToDimension(Blocks.RED_MUSHROOM_BLOCK, "mooshroom",  122, 32, 26));
+
+		portals.add(new PortalToDimension(Blocks.STRIPPED_BAMBOO_BLOCK, "bamboo_jungle",  104, 114, 32));
+		portals.add(new PortalToDimension(Blocks.STRIPPED_MANGROVE_LOG, "swamp",  91, 58, 35));
+		portals.add(new PortalToDimension(Blocks.STRIPPED_BIRCH_LOG, "birch",  162, 162, 130));
+		portals.add(new PortalToDimension(Blocks.STRIPPED_DARK_OAK_LOG, "dark_forest",  57, 40, 29));
 		portals.add(new PortalToDimension(Blocks.STRIPPED_CHERRY_LOG, "cherry_grove",  153, 75, 124));
-		portals.add(new PortalToDimension(Blocks.BAMBOO_BLOCK, "bamboo_jungle",  104, 114, 32));
 		portals.add(new PortalToDimension(Blocks.STRIPPED_ACACIA_LOG, "savanna",  147, 60, 23));
 		portals.add(new PortalToDimension(Blocks.STRIPPED_JUNGLE_LOG, "jungle",  147, 60, 23));
 
@@ -49,4 +51,8 @@ public class BiomeDimensionsExpanded implements ModInitializer {
 	}
 }
 
+/**
+ * @param portal_block the Minecraft block with which to make the portal from
+ * @param dimension_id string must exactly match the filename for the corresponding dimension in resources/data/<>/dimension
+ * */
 record PortalToDimension(Block portal_block, String dimension_id, int portal_r, int portal_g, int portal_b) {}
